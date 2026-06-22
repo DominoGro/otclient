71f1bf073cb05a73cf89607cf7a91dc1aecf6c34
Domino Gro <dominikgrochowski123@wp.pl>
2026-06-22 23:30:27 +0200

    Update item.h

--- src/client/item.h
+++ src/client/item.h
@@ -189,6 +189,7 @@ private:
     bool m_async{ true };
     bool m_hasAnimationPhases{ false };
     std::string m_tooltip;
+bool m_hasLight{ false };
  uint8_t m_animationPhases{ 0 };
     Animator* m_cachedIdleAnimator{ nullptr };
 #ifdef FRAMEWORK_EDITOR
